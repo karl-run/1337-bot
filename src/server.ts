7270ef3095a2a7506cb4c321201535e58c2743e1
Karl J. Overå <k@rl.run>
2023-05-25 22:08:56 +0200

    add message

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import { App } from "@slack/bolt";
 import { config } from "dotenv";
+import { getMilliseconds, getSeconds } from "date-fns/fp";
+import { format } from "date-fns";
+import nb from "date-fns/locale/nb";
 
 config();
 
@@ -9,8 +12,33 @@ const app = new App({
   socketMode: true,
 });
 
-app.message("1337", async ({ message, say }) => {
-  await say(`Hello ${message.ts}`);
+app.message("1337", async ({ message, say, event }) => {
+  if (message.type !== "message") return;
+
+  const time = new Date(+message.ts * 1000);
+
+  console.log(
+    `Received leet at ${time.toISOString()} by user ${(message as any).user}`
+  );
+
+  const isLeet = time.getHours() === 11 && time.getMinutes() === 37;
+  if (isLeet && time.getSeconds() === 0) {
+    await say(
+      `En ekte leetoo av <@${
+        (message as any).user
+      }>! :leetoo: Du var ${getMilliseconds(time)}ms over.`
+    );
+  } else if (isLeet) {
+    await say(
+      `Bra jobba <@${(message as any).user}>! Du var ${getSeconds(
+        time
+      )} sekunder inn i minuttet.`
+    );
+  } else {
+    await say(
+      `Ikke 1337. :letogun:`
+    );
+  }
 });
 
 (async () => {
