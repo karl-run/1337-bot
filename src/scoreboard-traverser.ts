f94733a189c1d3fa276334f2522495d71c24dcb2
Karl J. Overå <k@rl.run>
2023-09-09 10:42:40 +0200

    more computer science drafts [skip ci]

--- src/scoreboard-traverser.ts
+++ src/scoreboard-traverser.ts
@@ -1,12 +1,29 @@
 import * as R from "remeda";
-import { WebClient } from "@slack/web-api";
-import { ConversationsHistoryResponse } from "@slack/web-api";
+import { ConversationsHistoryResponse, WebClient } from "@slack/web-api";
+import { KnownBlock } from "@slack/bolt";
+import * as fs from "fs";
+import { formatDistanceToNow } from "date-fns";
+import { nb } from "date-fns/locale";
+import { createPermalink } from "./slack-utils";
 
 type Message = ConversationsHistoryResponse["messages"][number];
 
 type DateMessageTuple = readonly [Date, Message];
 
 export async function traverseHistory(channelId: string, client: WebClient) {
+  if (true) {
+    const jason: DateMessageTuple[] = JSON.parse(
+      fs.readFileSync("premature-leet-messages.json", "utf-8")
+    ).map(([date, message]: [Date, Message]) => [new Date(date), message]);
+
+    // console.log(jason);
+
+    // console.log(typeof jason[0][0]);
+
+    top10Leets(channelId, jason);
+    return;
+  }
+
   console.info("Fetching initial page");
 
   const traverser = nextPageTraverser(client, channelId);
@@ -22,10 +39,17 @@ export async function traverseHistory(channelId: string, client: WebClient) {
     ...restOfMessages,
   ]
     .map((it) => [new Date(+it.ts * 1000), it] as const)
-    .filter(([date]) => date.getHours() === 13 && date.getMinutes() === 37);
+    .filter(([date]) => date.getHours() === 13 && date.getMinutes() === 36);
 
-  console.log("Total count", allMessages.length);
-  createLeetDistributionPerSecondsCSV(allMessages);
+  /*
+  fs.writeFileSync(
+    "premature-leet-messages.json",
+    JSON.stringify(allMessages, null, 2)
+  );
+   */
+
+  // console.log("Total count", allMessages.length);
+  // createLeetDistributionPerSecondsCSV(allMessages);
 }
 
 function nextPageTraverser(client: WebClient, channelId: string) {
@@ -85,4 +109,46 @@ function createLeetDistributionPerSecondsCSV(messages: DateMessageTuple[]) {
   );
 
   console.log(countPerSecondsCSV);
+}
+
+function top10Leets(channelId: string, messages: DateMessageTuple[]) {
+  const top10 = R.pipe(
+    messages,
+    R.filter(([date]) => date.getSeconds() === 59),
+    R.sortBy([([date]) => 1000 - date.getMilliseconds(), "desc"]),
+    R.take(10)
+  );
+
+  console.log(JSON.stringify(createTop10Blocks(channelId, top10), null, 2));
+}
+
+function createTop10Blocks(
+  channelId: string,
+  messages: DateMessageTuple[]
+): KnownBlock[] {
+  const formatMessage = ([date, message]: DateMessageTuple): string =>
+    `-${(1000 - date.getMilliseconds()).toFixed(0).padStart(3, "0")}ms av <@${
+      message.user
+    }>: "${message.text}" ${formatDistanceToNow(date, {
+      locale: nb,
+      addSuffix: true,
+    })} (<${createPermalink(channelId, message.ts)}|meldingen>)`;
+
+  return [
+    {
+      type: "header",
+      text: {
+        type: "plain_text",
+        text: "Top 10 leetoos",
+        emoji: true,
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: messages.map(formatMessage).join("\n"),
+      },
+    },
+  ];
 }
---
+++ src/slack-utils.ts
@@ -0,0 +1,7 @@
+// Format:  https://systekkompis.slack.com/archives/<channel-id>/p<ts-without-period>
+export function createPermalink(channelId: string, ts: string) {
+  return `https://systekkompis.slack.com/archives/${channelId}/p${ts.replace(
+    ".",
+    ""
+  )}`;
+}
